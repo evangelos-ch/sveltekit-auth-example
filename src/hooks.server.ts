113249c86a54e6214c840c0f16aec2f1094594e8
Evangelos Ch <angelos.h@hotmail.com>
2023-01-19 15:50:41 +0000

    discord auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import Discord from "@auth/core/providers/discord"
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [Discord({ clientId: DISCORD_CLIENT_ID, clientSecret: DISCORD_CLIENT_SECRET })],
 })
